8e0936aec9af0e8047f5eba941b0850d6b91f082
Weverton Marques <wevertonms@gmail.com>
2020-02-18 00:40:25 -0300

    Add more fish shell settings.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
-set -x PATH ~/Downloads/Compressed/android-studio/bin/ ~/.local/bin/ ~/Downloads/flutter/bin /run/media/weverton/MY_FILES/TeXLive/bin/x86_64-linux/ $PATH
-set -x JAVA_HOME /usr/lib/jvm/default
-set -x ANDROID_HOME ~/Android/Sdk/
-set -x PATH $PATH $ANDROID_HOME/emulator
-set -x PATH $PATH $ANDROID_HOME/tools
-set -x PATH $PATH $ANDROID_HOME/tools/bin
-set -x PATH $PATH $ANDROID_HOME/platform-tools
+set -x PATH ~/.local/bin/ $PATH
+#set -x JAVA_HOME /usr/lib/jvm/default
+#set -x ANDROID_HOME ~/Android/Sdk/
+#set -x PATH $PATH $ANDROID_HOME/emulator
+#set -x PATH $PATH $ANDROID_HOME/tools
+#set -x PATH $PATH $ANDROID_HOME/tools/bin
+#set -x PATH $PATH $ANDROID_HOME/platform-tools
 set -g theme_display_virtualenv no  # Disables displaying the current virtualenv name
 
 alias config '/usr/bin/git --git-dir=/home/weverton/.dotfiles/ --work-tree=/home/weverton'
